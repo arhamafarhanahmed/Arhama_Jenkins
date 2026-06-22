e258747ac2596390ec9ff6b309b4832d4c53b4b3
arhamafarhanahmed <arhamafarhan256@gmail.com>
2026-06-22 13:27:51 +0500

    port 4000

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const http = require('http');
 http.createServer((req, res) => {
  res.writeHead(200);
  res.end('se231064\n');
-}).listen(4000, () => console.log('Running on port 3000'));
+}).listen(4000, () => console.log('Running on port 4000'));
