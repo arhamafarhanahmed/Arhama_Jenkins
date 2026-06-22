393db1f83db5c1d2991279c97a8ee4e911aaef70
arhamafarhanahmed <arhamafarhan256@gmail.com>
2026-06-22 15:41:20 +0500

    push4

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('http');
 http.createServer((req, res) => {
  res.writeHead(200);
- res.end('Auto trigerring the webhook Jenkins adn docker \n');
+ res.end('arhama farhan se231064\n');
 }).listen(4000, () => console.log('Running on port 4000'));
