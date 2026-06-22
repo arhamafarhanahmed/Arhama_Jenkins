133bcd95d4cc27b3da4d585e69a0ec5838614987
arhamafarhanahmed <arhamafarhan256@gmail.com>
2026-06-22 13:14:47 +0500

    Arhama

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('http');
 http.createServer((req, res) => {
  res.writeHead(200);
- res.end('Hello123 from Arhama se231064\n');
+ res.end('se231064\n');
 }).listen(4000, () => console.log('Running on port 3000'));
